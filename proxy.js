12869c77160c4c392cc197d2d8531db276a0862e
angelsl <angelsl@in04.sg>
2019-08-03 01:08:31 +0800

    Use Cloudflare worker to proxy

--- proxy.js
+++ proxy.js
@@ -1,44 +1,35 @@
-const http = require('http');
-const https = require('https');
-const url = require('url');
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
 
-function handle(req, res) {
-    if (req.method != "GET") {
-        res.writeHead(400);
-        res.end();
-        return;
-    }
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
+async function handleRequest(req) {
+  if (req.method != "GET") {
+    return new Response('', {status: 400});
+  }
 
-    let stop = url.parse(req.url).pathname.substring(1);
-    if (stop.toLowerCase() == "favicon.ico") {
-        res.writeHead(404);
-        res.end();
-        return;
-    }
+  let stop = new URL(req.url).pathname.substring(1);
+  if (stop.toLowerCase() == "favicon.ico") {
+    return new Response('', {status: 404});
+  }
 
-    let proxyUrl = stop
-        ? "https://nextbus.comfortdelgro.com.sg/eventservice.svc/Shuttleservice?busstopname=" + encodeURIComponent(decodeURIComponent(stop))
-        : "https://nextbus.comfortdelgro.com.sg/eventservice.svc/BusStops";
-    https.get(proxyUrl, proxyRes => {
-        if (proxyRes.statusCode !== 200) {
-            res.writeHead(proxyRes.statusCode);
-            res.end();
-            return;
-        }
+  let proxyUrl = stop
+    ? "https://nextbus.comfortdelgro.com.sg/eventservice.svc/Shuttleservice?busstopname=" + encodeURIComponent(decodeURIComponent(stop))
+    : "https://nextbus.comfortdelgro.com.sg/eventservice.svc/BusStops";
+  let proxyRes = await fetch(proxyUrl);
 
-        res.setHeader('Access-Control-Allow-Origin', 'https://angelsl.github.io');
-        if (proxyRes.headers['content-type']) {
-            res.setHeader('Content-Type', proxyRes.headers['content-type']);
-        }
+  if (proxyRes.status !== 200) {
+      return new Response('', {status: proxyRes.status});
+  }
 
-        proxyRes.setEncoding('utf8');
-        let rawData = '';
-        proxyRes.on('data', (chunk) => { rawData += chunk; });
-        proxyRes.on('end', () => {
-            res.end(rawData, 'utf8');
-        });
-    });
+  let ret = new Response(proxyRes.body);
+  ret.headers.append('Access-Control-Allow-Origin', 'https://angelsl.github.io');
+  if (proxyRes.headers.has('Content-Type')) {
+      ret.headers.append('Content-Type', proxyRes.headers.get('Content-Type'));
+  }
+
+  return ret;
 }
-
-let server = http.createServer(handle);
-server.listen(process.env.PORT || 8080);
